6fcba752ea3a61828bd190eb26efae0d5ac56bbb
Tomer <vardi.tomer@gmail.com>
2025-10-22 16:38:16 -0700

    implement echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        boolean isRunning = true;
+
+        while (isRunning){
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            String command = input.split(" ")[0];
+
+            switch (command) {
+                case "exit" -> System.exit(0);
+                case "echo" -> System.out.println(input.substring(4));
+                default -> System.err.printf("%s: input not found%n", input);
+            }
+        }
+    }
 }
